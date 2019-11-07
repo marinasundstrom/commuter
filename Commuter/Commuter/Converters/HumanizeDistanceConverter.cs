707a76d7237f44bedb56e96ad0446314d19b68e9
Robert Sundström <robert.sundstrom@outlook.com>
2019-11-07 11:19:01 +0100

    Change header colors and distance display

---
+++ Commuter/Commuter/Converters/HumanizeDistanceConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+using Xamarin.Forms;
+
+namespace Commuter.Converters
+{
+    internal class HumanizeDistanceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var distance = (int)value;
+
+            if (distance == 0)
+            {
+                return "Here";
+            }
+
+            return distance > 1 ? $"{distance} meters" : $"1 meter";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
