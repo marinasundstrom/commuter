9350b4438dc1ba13543592b8c3007012cea664b2
Robert Sundström <robert.sundstrom@outlook.com>
2019-11-11 17:16:07 +0100

    StopPoint starting with a number is a “Track” #34

---
+++ Commuter/Commuter/Converters/StopPointNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+using Xamarin.Forms;
+
+namespace Commuter.Converters
+{
+    internal class StopPointNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string name)
+            {
+                if (char.IsDigit(name[0]))
+                {
+                    return $"Track {name}";
+                }
+
+                return $"Position {name}";
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
