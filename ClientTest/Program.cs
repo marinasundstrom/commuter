2745b5f265831276317ae056ece70d79c95bf742
Robert Sundström <robert.sundstrom@outlook.com>
2019-11-11 19:53:38 +0100

    Add client test project

---
+++ ClientTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Commuter;
+
+namespace ClientTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            OpenApiClient client = new OpenApiClient(new System.Net.Http.HttpClient());
+            var c = await client.GetNearestStopAreasAsync(55.608975, 12.9985393, 400);
+            Console.WriteLine(c.GetType());
+        }
+    }
+}
